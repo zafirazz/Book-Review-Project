7d2a5bc0b4ae954d2b90ddf905cf7d1ac4761e2a
Julia_el_masri <julia.el.masri@inf.unideb.hu>
2024-12-05 14:58:23 +0100

    Added review feature for each book and for users to see reviews

--- login.py
+++ login.py
@@ -1,20 +1,17 @@
-"""first: get login data from login.html
-second: create database containing id, username, password and review
-third: add username, password to the database"""
 import logging
 from functools import wraps
-from flask import g, request, redirect, url_for, session, render_template, Flask, jsonify
+from flask import request, redirect, url_for, session, render_template, Flask, jsonify
 from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from db import db
-from models import Book, User_Data
+from models import Book, User_Data, Review
 
 
-engine = create_engine('postgresql+psycopg2://alishba:alishba@localhost:5432/postgres')
+engine = create_engine('postgresql+psycopg2://julia:new_password@localhost:5432/postgres')
 
 app = Flask(__name__, template_folder='front-end', static_folder='front-end/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://alishba:alishba@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://julia:new_password@localhost:5432/postgres'
 app.secret_key=["hioergerhgoierhgierhogiehgoieagawoeigyireyg"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -108,33 +105,48 @@ def search():
 
     return render_template('search.html', query=query, results=results)
 
-@app.route('/front-end/read_review', methods=['GET', 'POST'])
-def read_review():
-    logging.debug("i am in review function")
-    books = Book.query.all()
-    return render_template('read_review.html', books=books)
-
-@app.route('/front-end/logout', methods=['GET', 'POST'])
-def logout():
-    # TODO
-    return 0
 
 @app.route('/front-end/review', methods=['GET', 'POST'])
-
 @login_required
 def review():
     logging.debug("i am in review function")
     books = Book.query.all()
     return render_template('review.html', books=books)
 
-@app.route('/front-end/book_page')
+
+@app.route('/front-end/book_page',  methods=['GET', 'POST'])
 def book_page():
     book_isbn = request.args.get('book')
-
+    username = session['username']
+    userid = session['user_id']
     book = Book.query.filter_by(isbn=book_isbn).first()
-    # Do something with book_isbn, like fetching book details
-    return render_template('book_page.html', book=book)
+    reviewed = db.session.query(Review).filter_by(isbn=book.isbn, user_id=userid).first()
+    if not reviewed and request.method == "POST":
+        review = request.form.get("review-written")
+        if review:
+            new_review = Review(isbn=book.isbn, user_id=userid, review=review)
+            db.session.add(new_review)
+            db.session.commit()
+    if reviewed and request.method == "POST":
+        #db.session.delete(reviewed)
+        #db.session.commit()
+        return login_error("you already reviewed this book", 401)
+    return render_template('book_page.html', book=book, username=username)
 
+
+@app.route('/front-end/read_review', methods=['GET', 'POST'])
+def read_review():
+    logging.debug("i am in review function")
+    books = Book.query.all()
+    return render_template('read_review.html', books=books)
+
+
+@app.route('/front-end/read_reviews', methods=['GET', 'POST'])
+def read_reviews():
+   book_isbn = request.args.get('book')
+   book = Book.query.filter_by(isbn=book_isbn).first()
+   reviews = db.session.query(Review, User_Data).join(User_Data, Review.user_id == User_Data.ID).filter(Review.isbn==book_isbn).all()
+   return render_template('read_reviews.html', book=book, reviews=reviews)
 
 def initialize_database():
     with app.app_context():
